05a68e205e3b0e72dd42362cc6587318f72878d7
VishwaSoundararajan <vishwasoundararajan@gmail.com>
2024-03-04 12:38:27 +0530

    changed build gradle, podspec

---
+++ lib/rewebsdk.dart
@@ -0,0 +1,15 @@
+import 'dart:html' as html;
+import 'dart:js' as js;
+
+class ReFlutterWeb {
+  static final sdk = js.context['web'];
+  static final obj = js.JsObject(js.context['Object']);
+
+  static void initWebSDK(String fcmPath) {
+    html.Element? head = html.document.querySelector('head');
+    html.ScriptElement script = html.ScriptElement();
+    script.defer = true;
+    script.src =
+    "https://web.rss.io/handlers/5g7a2e8e_1bdb_9d28_278a2759dfe3c.sdk";
+  }
+}
