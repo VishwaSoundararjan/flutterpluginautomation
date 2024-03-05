1a0c0c6896aba697bc00b663bb1b840c11f7f59f
VishwaSoundararjan <vishwasoundararajan@gmail.com>
2024-03-05 15:17:57 +0530

    reflutter web error fix

--- lib/rewebsdk.dart
+++ lib/rewebsdk.dart
@@ -9,7 +9,6 @@ class ReFlutterWeb {
     html.Element? head = html.document.querySelector('head');
     html.ScriptElement script = html.ScriptElement();
     script.defer = true;
-    script.src =
-    "https://web.rss.io/handlers/5g7a2e8e_1bdb_9d28_278a2759dfe3c.sdk";
+    script.src = "https://web.rss.io/handlers/5g7a2e8e_1bdb_9d28_278a2759dfe3c.sdk";
   }
 }
